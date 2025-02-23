9d5ef05e9c65bda10a38be16d614b6fdba5a3858
ITzDivyanshu08 <divyanshubarthwal08@gmail.com>
2025-02-23 17:48:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function handleClick(option) {
+    switch(option) {
+        case 1:
+            alert("Pterodactyl Setup Selected!");
+            break;
+        case 2:
+            alert("Hydra Panel Installation Selected!");
+            break;
+        case 3:
+            alert("Making VPS 24/7 Selected!");
+            break;
+        default:
+            alert("Unknown option!");
+    }
+}
